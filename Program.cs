7ec12781f420524ee575033fe2554aedd63b550d
gabriel-vernilo <bielvernilo@gmail.com>
2020-03-03 19:13:23 -0300

    Visual Studio file

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double Primeiro_numero, Segundo_numero, res;
+            String Nome;
+            Console.WriteLine("======CALCULO DE MÉDIA ARITMÉTICA======");
+            Console.WriteLine("\nQual é seu nome?: ");
+            Nome = Console.ReadLine();
+            Console.WriteLine("digite o primiero número: ");
+            Primeiro_numero = Double.Parse(Console.ReadLine());
+            Console.WriteLine("digite o segundo número:");
+            Segundo_numero = Double.Parse(Console.ReadLine());
+            res = (Primeiro_numero + Segundo_numero) / 2;
+            Console.WriteLine("======RESULTADO======");
+            Console.Write($" {Nome}, sua média é {res} \n");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
